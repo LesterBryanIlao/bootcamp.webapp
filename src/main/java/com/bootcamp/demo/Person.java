71227b754266e497804948bc7af7b560365d4ce4
venerguevarra <vener.guevarra@gmail.com>
2022-11-03 23:45:54 -0700

    Added person java bean

---
+++ src/main/java/com/bootcamp/demo/Person.java
@@ -0,0 +1,32 @@
+package com.bootcamp.demo;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
